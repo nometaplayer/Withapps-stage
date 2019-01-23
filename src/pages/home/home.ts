c46d4497a873cde7c622db4c2e35a63af68fb982
Kévin <etkevin85@hotmail.fr>
2019-01-23 14:15:34 +0100

    test
    mise en place du git

---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { HomePage } from './home';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Page1 } from '../Page1/home';
+@Component({
+  
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+  
+    
+  }
+  goToHome(){
+    this.navCtrl.setRoot(Page1);
+  }
+
+}
